42310dbd51588fa7def52635704f2d2f492b912e
David Sugar <dyfet@gnutelephony.org>
2006-07-20 02:29:06 +0000

    adpcm fix

--- codecs/g721.cpp
+++ codecs/g721.cpp
@@ -37,29 +37,145 @@
 // If you write modifications of your own for ccaudio, it is your choice
 // whether to permit this exception to apply to your modifications.
 // If you do not wish that, delete this exception notice.
+//
+// Based on freely licensed g72x codec code from Sun Microsystems, Inc.
+
 
 #include "codecs.h"
 
 namespace ccAudioCodec {
 using namespace ost;
 
-static int index[8] = { -1, -1, -1, -1, 2, 4, 6, 8 };
+static short power2[15] = {1, 2, 4, 8, 0x10, 0x20, 0x40, 0x80,
+                        0x100, 0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000};
 
-static int steps[49] = {
-  16, 17, 19, 21, 23, 25, 28, 31, 34, 37, 41, 45, 50, 55, 60, 66, 73,
-  80, 88, 97, 107, 118, 130, 143, 157, 173, 190, 209, 230, 253, 279,
-  307, 337, 371, 408, 449, 494, 544, 598, 658, 724, 796, 876, 963,
-  1060, 1166, 1282, 1411, 1552
-};
+static short qtab_721[7] = {-124, 80, 178, 246, 300, 349, 400};
+
+static short    _dqlntab[16] = {-2048, 4, 135, 213, 273, 323, 373, 425,
+                                425, 373, 323, 273, 213, 135, 4, -2048};
+
+static short    _witab[16] = {-12, 18, 41, 64, 112, 198, 355, 1122,
+                                1122, 355, 198, 112, 64, 41, 18, -12};
+
+static short    _fitab[16] = {0, 0, 0, 0x200, 0x200, 0x200, 0x600, 0xE00,
+                                0xE00, 0x600, 0x200, 0x200, 0x200, 0, 0, 0};
+
+static int quan(
+	int		val,
+	short		*table,
+	int		size)
+{
+	int		i;
+
+	for (i = 0; i < size; i++)
+		if (val < *table++)
+			break;
+	return (i);
+}
+
+int quantize(
+	int		d,	/* Raw difference signal sample */
+	int		y,	/* Step size multiplier */
+	short		*table,	/* quantization table */
+	int		size)	/* table size of short integers */
+{
+	short		dqm;	/* Magnitude of 'd' */
+	short		exp;	/* Integer part of base 2 log of 'd' */
+	short		mant;	/* Fractional part of base 2 log */
+	short		dl;	/* Log of magnitude of 'd' */
+	short		dln;	/* Step size scale factor normalized log */
+	int		i;
+
+	/*
+	 * LOG
+	 *
+	 * Compute base 2 log of 'd', and store in 'dl'.
+	 */
+	dqm = abs(d);
+	exp = quan(dqm >> 1, power2, 15);
+	mant = ((dqm << 7) >> exp) & 0x7F;	/* Fractional portion. */
+	dl = (exp << 7) + mant;
+
+	/*
+	 * SUBTB
+	 *
+	 * "Divide" by step size multiplier.
+	 */
+	dln = dl - (y >> 2);
+
+	/*
+	 * QUAN
+	 *
+	 * Obtain codword i for 'd'.
+	 */
+	i = quan(dln, table, size);
+	if (d < 0)			/* take 1's complement of i */
+		return ((size << 1) + 1 - i);
+	else if (i == 0)		/* take 1's complement of 0 */
+		return ((size << 1) + 1); /* new in 1988 */
+	else
+		return (i);
+}
+
+static int fmult(
+	int		an,
+	int		srn)
+{
+	short		anmag, anexp, anmant;
+	short		wanexp, wanmant;
+	short		retval;
+
+	anmag = (an > 0) ? an : ((-an) & 0x1FFF);
+	anexp = quan(anmag, power2, 15) - 6;
+	anmant = (anmag == 0) ? 32 :
+	    (anexp >= 0) ? anmag >> anexp : anmag << -anexp;
+	wanexp = anexp + ((srn >> 6) & 0xF) - 13;
+
+	wanmant = (anmant * (srn & 077) + 0x30) >> 4;
+	retval = (wanexp >= 0) ? ((wanmant << wanexp) & 0x7FFF) :
+	    (wanmant >> -wanexp);
+
+	return (((an ^ srn) < 0) ? -retval : retval);
+}
+
+static int reconstruct(
+	int		sign,	/* 0 for non-negative value */
+	int		dqln,	/* G.72x codeword */
+	int		y)	/* Step size multiplier */
+{
+	short		dql;	/* Log of 'dq' magnitude */
+	short		dex;	/* Integer part of log */
+	short		dqt;
+	short		dq;	/* Reconstructed difference signal sample */
+
+	dql = dqln + (y >> 2);	/* ADDA */
+
+	if (dql < 0) {
+		return ((sign) ? -0x8000 : 0);
+	} else {		/* ANTILOG */
+		dex = (dql >> 7) & 15;
+		dqt = 128 + (dql & 127);
+		dq = (dqt << 7) >> (14 - dex);
+		return ((sign) ? (dq - 0x8000) : dq);
+	}
+}
 
 class g721Codec : public AudioCodec
 {
 private:
 	typedef struct state
 	{
-		int ssindex;
-		int signal;
-		int next_flag;
+		long yl;
+		short yu;
+		short dms;
+		short dml;
+		short ap;
+		short a[2];
+		short b[6];
+		short pk[2];
+		short dq[6];
+		short sr[2];
+		char td;
 	}	state_t;
 
 	state_t encode_state, decode_state;
@@ -68,9 +184,14 @@ public:
 	AudioCodec *getByInfo(Info &info);
 	AudioCodec *getByFormat(const char *format);
 
+	void update(int cs, int y, int wi, int fi, int dq, int sr, int dqsez, state_t *state);
 	unsigned decode(Linear buffer, void *from, unsigned lsamples);
 	unsigned encode(Linear buffer, void *dest, unsigned lsamples);
 	short coder(state_t *state, int nib);
+	unsigned char encoder(short sl, state_t *state);
+	int predictor_zero(state_t *state);
+	int predictor_pole(state_t *state);
+	int step_size(state_t *state);
 
 	g721Codec(const char *id, Encoding e);
 	g721Codec();
@@ -79,6 +200,8 @@ public:
 
 g721Codec::g721Codec() : AudioCodec()
 {
+	unsigned pos;
+
 	info.framesize = 1;
 	info.framecount = 2;
 	info.rate = 8000;
@@ -88,6 +211,12 @@ g721Codec::g721Codec() : AudioCodec()
 
 	memset(&encode_state, 0, sizeof(encode_state));
         memset(&decode_state, 0, sizeof(decode_state));
+	encode_state.yl = decode_state.yl = 34816;
+	encode_state.yu = decode_state.yu = 544;
+	encode_state.sr[0] = encode_state.sr[1] = decode_state.sr[0] = decode_state.sr[1] = 32;
+
+	for(pos = 0; pos < 6; ++pos)
+		encode_state.dq[pos] = decode_state.dq[pos] = 32;
 }
 
 g721Codec::g721Codec(const char *id, Encoding e) : AudioCodec(id, e)
@@ -102,81 +231,292 @@ g721Codec::g721Codec(const char *id, Encoding e) : AudioCodec(id, e)
 g721Codec::~g721Codec()
 {}
 
-short g721Codec::coder(state_t *state, int nib)
+void g721Codec::update(
+	int		code_size,	/* distinguish 723_40 with others */
+	int		y,		/* quantizer step size */
+	int		wi,		/* scale factor multiplier */
+	int		fi,		/* for long/short term energies */
+	int		dq,		/* quantized prediction difference */
+	int		sr,		/* reconstructed signal */
+	int		dqsez,		/* difference from 2-pole predictor */
+	state_t *state_ptr)		/* coder state pointer */
 {
-	int step, sign, diff;
+	int		cnt;
+	short		mag, exp;	/* Adaptive predictor, FLOAT A */
+	short		a2p = 0;		/* LIMC */
+	short		a1ul;		/* UPA1 */
+	short		pks1;		/* UPA2 */
+	short		fa1;
+	char		tr;		/* tone/transition detector */
+	short		ylint, thr2, dqthr;
+	short  		ylfrac, thr1;
+	short		pk0;
 
-	step = steps[state->ssindex];
-	sign = nib & 0x08;
-	nib &= 0x07;
+	pk0 = (dqsez < 0) ? 1 : 0;	/* needed in updating predictor poles */
 
-	diff = (((nib << 1) + 1) * step) >> 3;
-		
-	if(sign)
-		diff = -diff;
+	mag = dq & 0x7FFF;		/* prediction difference magnitude */
+	/* TRANS */
+	ylint = state_ptr->yl >> 15;	/* exponent part of yl */
+	ylfrac = (state_ptr->yl >> 10) & 0x1F;	/* fractional part of yl */
+	thr1 = (32 + ylfrac) << ylint;		/* threshold */
+	thr2 = (ylint > 9) ? 31 << 10 : thr1;	/* limit thr2 to 31 << 10 */
+	dqthr = (thr2 + (thr2 >> 1)) >> 1;	/* dqthr = 0.75 * thr2 */
+	if (state_ptr->td == 0)		/* signal supposed voice */
+		tr = 0;
+	else if (mag <= dqthr)		/* supposed data, but small mag */
+		tr = 0;			/* treated as voice */
+	else				/* signal is data (modem) */
+		tr = 1;
 
-	if(state->next_flag & 0x01)
-		state->signal -= 8;
-	else if(state->next_flag & 0x02)
-		state->signal += 8;
+	/*
+	 * Quantizer scale factor adaptation.
+	 */
 
-	state->signal += diff;
-	if (state->signal > 2047)
-                state->signal = 2047;
-	else if (state->signal < -2047)
-               	state->signal = -2047;
+	/* FUNCTW & FILTD & DELAY */
+	/* update non-steady state step size multiplier */
+	state_ptr->yu = y + ((wi - y) >> 5);
 
-        state->next_flag = 0;
-	state->ssindex += index[nib];
-	if (state->ssindex < 0)
-		state->ssindex = 0;
-	else if(state->ssindex > 48)
-		state->ssindex = 48;
+	/* LIMB */
+	if (state_ptr->yu < 544)	/* 544 <= yu <= 5120 */
+		state_ptr->yu = 544;
+	else if (state_ptr->yu > 5120)
+		state_ptr->yu = 5120;
 
-	return state->signal << 4;	
+	/* FILTE & DELAY */
+	/* update steady state step size multiplier */
+	state_ptr->yl += state_ptr->yu + ((-state_ptr->yl) >> 6);
+
+	/*
+	 * Adaptive predictor coefficients.
+	 */
+	if (tr == 1) {			/* reset a's and b's for modem signal */
+		state_ptr->a[0] = 0;
+		state_ptr->a[1] = 0;
+		state_ptr->b[0] = 0;
+		state_ptr->b[1] = 0;
+		state_ptr->b[2] = 0;
+		state_ptr->b[3] = 0;
+		state_ptr->b[4] = 0;
+		state_ptr->b[5] = 0;
+	} else {			/* update a's and b's */
+		pks1 = pk0 ^ state_ptr->pk[0];		/* UPA2 */
+
+		/* update predictor pole a[1] */
+		a2p = state_ptr->a[1] - (state_ptr->a[1] >> 7);
+		if (dqsez != 0) {
+			fa1 = (pks1) ? state_ptr->a[0] : -state_ptr->a[0];
+			if (fa1 < -8191)	/* a2p = function of fa1 */
+				a2p -= 0x100;
+			else if (fa1 > 8191)
+				a2p += 0xFF;
+			else
+				a2p += fa1 >> 5;
+
+			if (pk0 ^ state_ptr->pk[1])
+				/* LIMC */
+				if (a2p <= -12160)
+					a2p = -12288;
+				else if (a2p >= 12416)
+					a2p = 12288;
+				else
+					a2p -= 0x80;
+			else if (a2p <= -12416)
+				a2p = -12288;
+			else if (a2p >= 12160)
+				a2p = 12288;
+			else
+				a2p += 0x80;
+		}
+
+		/* TRIGB & DELAY */
+		state_ptr->a[1] = a2p;
+
+		/* UPA1 */
+		/* update predictor pole a[0] */
+		state_ptr->a[0] -= state_ptr->a[0] >> 8;
+		if (dqsez != 0)
+			if (pks1 == 0)
+				state_ptr->a[0] += 192;
+			else
+				state_ptr->a[0] -= 192;
+
+		/* LIMD */
+		a1ul = 15360 - a2p;
+		if (state_ptr->a[0] < -a1ul)
+			state_ptr->a[0] = -a1ul;
+		else if (state_ptr->a[0] > a1ul)
+			state_ptr->a[0] = a1ul;
+
+		/* UPB : update predictor zeros b[6] */
+		for (cnt = 0; cnt < 6; cnt++) {
+			if (code_size == 5)		/* for 40Kbps G.723 */
+				state_ptr->b[cnt] -= state_ptr->b[cnt] >> 9;
+			else			/* for G.721 and 24Kbps G.723 */
+				state_ptr->b[cnt] -= state_ptr->b[cnt] >> 8;
+			if (dq & 0x7FFF) {			/* XOR */
+				if ((dq ^ state_ptr->dq[cnt]) >= 0)
+					state_ptr->b[cnt] += 128;
+				else
+					state_ptr->b[cnt] -= 128;
+			}
+		}
+	}
+
+	for (cnt = 5; cnt > 0; cnt--)
+		state_ptr->dq[cnt] = state_ptr->dq[cnt-1];
+	/* FLOAT A : convert dq[0] to 4-bit exp, 6-bit mantissa f.p. */
+	if (mag == 0) {
+		state_ptr->dq[0] = (dq >= 0) ? 0x20 : 0xFC20;
+	} else {
+		exp = quan(mag, power2, 15);
+		state_ptr->dq[0] = (dq >= 0) ?
+		    (exp << 6) + ((mag << 6) >> exp) :
+		    (exp << 6) + ((mag << 6) >> exp) - 0x400;
+	}
+
+	state_ptr->sr[1] = state_ptr->sr[0];
+	/* FLOAT B : convert sr to 4-bit exp., 6-bit mantissa f.p. */
+	if (sr == 0) {
+		state_ptr->sr[0] = 0x20;
+	} else if (sr > 0) {
+		exp = quan(sr, power2, 15);
+		state_ptr->sr[0] = (exp << 6) + ((sr << 6) >> exp);
+	} else if (sr > -32768) {
+		mag = -sr;
+		exp = quan(mag, power2, 15);
+		state_ptr->sr[0] =  (exp << 6) + ((mag << 6) >> exp) - 0x400;
+	} else
+		state_ptr->sr[0] = 0xFC20;
+
+	/* DELAY A */
+	state_ptr->pk[1] = state_ptr->pk[0];
+	state_ptr->pk[0] = pk0;
+
+	/* TONE */
+	if (tr == 1)		/* this sample has been treated as data */
+		state_ptr->td = 0;	/* next one will be treated as voice */
+	else if (a2p < -11776)	/* small sample-to-sample correlation */
+		state_ptr->td = 1;	/* signal may be data */
+	else				/* signal is voice */
+		state_ptr->td = 0;
+
+	/*
+	 * Adaptation speed control.
+	 */
+	state_ptr->dms += (fi - state_ptr->dms) >> 5;		/* FILTA */
+	state_ptr->dml += (((fi << 2) - state_ptr->dml) >> 7);	/* FILTB */
+
+	if (tr == 1)
+		state_ptr->ap = 256;
+	else if (y < 1536)					/* SUBTC */
+		state_ptr->ap += (0x200 - state_ptr->ap) >> 4;
+	else if (state_ptr->td == 1)
+		state_ptr->ap += (0x200 - state_ptr->ap) >> 4;
+	else if (abs((state_ptr->dms << 2) - state_ptr->dml) >=
+	    (state_ptr->dml >> 3))
+		state_ptr->ap += (0x200 - state_ptr->ap) >> 4;
+	else
+		state_ptr->ap += (-state_ptr->ap) >> 4;
+}
+
+int g721Codec::predictor_zero(
+	state_t *state_ptr)
+{
+	int		i;
+	int		sezi;
+
+	sezi = fmult(state_ptr->b[0] >> 2, state_ptr->dq[0]);
+	for (i = 1; i < 6; i++)			/* ACCUM */
+		sezi += fmult(state_ptr->b[i] >> 2, state_ptr->dq[i]);
+	return (sezi);
+}
+
+int g721Codec::predictor_pole(
+	state_t *state_ptr)
+{
+	return (fmult(state_ptr->a[1] >> 2, state_ptr->sr[1]) +
+	    fmult(state_ptr->a[0] >> 2, state_ptr->sr[0]));
+}
+
+int g721Codec::step_size(
+	state_t *state_ptr)
+{
+	int		y;
+	int		dif;
+	int		al;
+
+	if (state_ptr->ap >= 256)
+		return (state_ptr->yu);
+	else {
+		y = state_ptr->yl >> 6;
+		dif = state_ptr->yu - y;
+		al = state_ptr->ap >> 2;
+		if (dif > 0)
+			y += (dif * al) >> 6;
+		else if (dif < 0)
+			y += (dif * al + 0x3F) >> 6;
+		return (y);
+	}
+}
+
+unsigned char g721Codec::encoder(short sl, state_t *state)
+{
+	short sezi, se, sez;
+	short d, sr, y, dqsez, dq, i;
+
+	sl >>= 2;
+
+        sezi = predictor_zero(state);
+        sez = sezi >> 1;
+        se = (sezi + predictor_pole(state)) >> 1; 
+
+        d = sl - se;
+
+        y = step_size(state);
+        i = quantize(d, y, qtab_721, 7);
+        dq = reconstruct(i & 8, _dqlntab[i], y);
+        sr = (dq < 0) ? se - (dq & 0x3FFF) : se + dq;
+
+        dqsez = sr + sez - se; 
+
+        update(4, y, _witab[i] << 5, _fitab[i], dq, sr, dqsez, state);
+
+        return (i);
+}
+
+short g721Codec::coder(state_t *state, int i)
+{
+	short sezi, sei, sez, se;
+	short y, sr, dq, dqsez;
+
+	sezi = predictor_zero(state);
+	sez = sezi >> 1;
+	sei = sezi + predictor_pole(state);
+	se = sei >> 1;
+	y = step_size(state);
+	dq = reconstruct(i & 0x08, _dqlntab[i], y);
+	sr = (dq < 0) ? (se - (dq & 0x3fff)) : se + dq;
+	dqsez = sr - se + sez;
+	update(4, y, _witab[i] << 5, _fitab[i], dq, sr, dqsez, state);
+	return sr << 2;
 }
 
 unsigned g721Codec::encode(Linear buffer, void *coded, unsigned lsamples)
 {
 	unsigned count = (lsamples / 2) * 2;
-	short data;
-	bool hi = false;
-	int diff, step, nib;
 	unsigned char byte = 0;
 	Encoded dest = (Encoded)coded;
 
+
 	while(count--)
 	{
-		data = (*(buffer++)) >> 4;
-		step = steps[encode_state.ssindex];
-		diff = data - encode_state.signal;
-
-		if(diff < 0)
+		byte = encoder(*(buffer++), &encode_state) << 4;
+		if(count)
 		{
-			nib = (-diff << 2) / step;
-			if(nib > 7)
-				nib = 7;
-			nib |= 0x08;
+			--count;
+			byte |= encoder(*(buffer++), &encode_state);
 		}
-		else
-		{
-			nib = (diff << 2) / step;
-			if(nib > 7)
-				nib = 7;
-		}
-		coder(&encode_state, nib);
-		if(hi)
-		{
-			byte |= nib;
-			*(dest++) = byte;
-			hi = false;
-		}
-		else
-		{
-			byte = (unsigned char)(nib << 4);
-			hi = true;
-		}
-				
+		*(dest++) = byte;				
 	}
 	return (lsamples / 2) * 2;
 }
